44f7592f63ff543b1472530ce82bece3e0986433
Eric Carr <superjapanfreak@gmail.com>
2012-05-11 21:11:33 -0700

    made the compare-calls macro more robust

--- sandbox.clj
+++ sandbox.clj
@@ -1,10 +1,5 @@
 (ns sandbox
-  (:require [clojure.string :as string])) ; test
-
-(def ^:dynamic v 10)
-
-(defn f1 []
-  (println "f1: v " v))
+  (:require [clojure.string :as string]))
 
 (def ex_vector [1 2 3])
 (def ex_list '(1 2 3))
@@ -32,8 +27,6 @@
   (if (empty? vct) 0.0
     (/ (apply + vct) (count vct))))
 
-; (let [[x :x y :y] {:x 10 :y 10}] (+ x y))
-
 (defn ellipsize [words]
   (let [split_words (string/split words #"\s+")]
     (str (string/join " " (take 3 split_words)) "...")))
@@ -50,10 +43,23 @@
   ([] #{})
   ([& others] (set (apply vector others))))
 
-(defn to-vector
-  ([values] (if (vector? values) values (apply vector values))))
-
 (defn apply*
+<<<<<<< HEAD
+  "My implementation of apply"
+  [f vs]
+  (loop [acc (f (first vs)), vs (rest vs)]
+    (if (seq vs)
+      (recur (f acc (first vs)) (rest vs))
+      acc)
+    ))
+
+(defmacro compare-calls [fs & values]
+  `(loop [fs# ~fs]
+     (when (seq fs#)
+       (print "Calling" (first fs#) ": ")
+       (time ((first fs#) ~@values))
+       (recur (rest fs#)))))
+=======
  "My implementation of apply"
  [f vs]
  (loop [acc (f (first vs)), vs (rest vs)]
@@ -67,16 +73,44 @@
   `(do
      (time (~f1 ~@vs))
      (time (~f2 ~@vs))))
+>>>>>>> 2725d4113d3bfd9bbe5f64020d0af0fb2f6063a4
 
 (def muted-writer-buf ())
 
 (def muted-writer ; Only works for whatever was printed last unfortunately
- (proxy [java.io.Writer] [] ; latter vector is vector of arguments for superclass constructor
-  (write [buf] (def muted-writer-buf (concat muted-writer-buf buf)))
-  (close [] nil)
-  (flush [] nil)))
+  (proxy [java.io.Writer] [] ; latter vector is vector of arguments for superclass constructor
+    (write [buf] (def muted-writer-buf (concat muted-writer-buf buf)))
+    (close [] nil)
+    (flush [] nil)))
 
 (defmacro mute-print 
+<<<<<<< HEAD
+  [form]
+  `(binding [*out* muted-writer]
+     (list ~form ~(apply str muted-writer-buf))))
+
+
+
+(defprotocol Stringifiable
+  (stringify [object] "Return a string version of the object"))
+
+(extend java.util.Date
+  Stringifiable
+  {:stringify (fn [date] (.toString date))})
+
+(def x 10)
+
+
+(comment 
+  (def frame (java.awt.Frame.))
+
+  (defn find-java-method [jclassinst name]
+    (for [method (.getMethods jclassinst)
+          :let [methodName (.getName method)]
+          :when])))
+
+=======
  [form]
  `(binding [*out* muted-writer]
    (list ~form (~apply ~str muted-writer-buf))))
+>>>>>>> 2725d4113d3bfd9bbe5f64020d0af0fb2f6063a4
