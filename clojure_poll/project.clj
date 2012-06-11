1f424639c5ab5e392ef06aea11cfb036cf6dbe49
Eric Carr <superjapanfreak@gmail.com>
2012-06-11 12:46:08 -0700

    added hiccup and changed the noir dependency from 1.2.1 to 1.3.0-beta1

--- clojure_poll/project.clj
+++ clojure_poll/project.clj
@@ -1,6 +1,7 @@
 (defproject clojure_poll "0.0.1-SNAPSHOT"
             :description "A poll web application written in clojure using noir"
             :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.2.1"]]
+                           [hiccup "1.0.0"]
+                           [noir "1.3.0-beta1"]]
             :main clojure_poll.server)
 
