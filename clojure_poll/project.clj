a7813b4168d4c0ee6ae4616765127359ef5e0246
Eric Carr <superjapanfreak@gmail.com>
2012-06-11 06:16:00 -0700

    change dependency from clojure 1.3.0 to 1.4.0

--- clojure_poll/project.clj
+++ clojure_poll/project.clj
@@ -1,6 +1,6 @@
 (defproject clojure_poll "0.0.1-SNAPSHOT"
             :description "A poll web application written in clojure using noir"
-            :dependencies [[org.clojure/clojure "1.3.0"]
+            :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.2.1"]]
             :main clojure_poll.server)
 
