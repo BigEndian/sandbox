5a87abbe0b25286df1932fff85dc49e6632407f1
Eric Carr <superjapanfreak@gmail.com>
2012-06-11 17:04:58 -0700

    added quite a few pages, not usable

--- clojure_poll/src/clojure_poll/views/common.clj
+++ clojure_poll/src/clojure_poll/views/common.clj
@@ -1,12 +1,61 @@
 (ns clojure_poll.views.common
-  (:use [noir.core :only [defpartial]]
-        [hiccup.page-helpers :only [include-css html5]]))
+  (:use [noir.core]
+        [noir.response :only [redirect]]
+        [hiccup.page :only [include-css html5]]
+        [hiccup.form :only [text-field label form-to submit-button]])
+  (:require [clojure_poll.models.poll :as poll]))
 
 (defpartial layout [& content]
             (html5
               [:head
-               [:title "clojure_poll"]
+               [:title "Clojure Poll"]
                (include-css "/css/reset.css")]
               [:body
+               [:h1.header "Clojure Poll"]
                [:div#wrapper
                 content]]))
+
+(defpartial polls-list [polls]
+            (for [poll polls]
+              [:pre.poll-title (str (:id @poll) \space (:title @poll) \newline (str @poll))]))
+
+(defpartial poll-list [poll]
+            [[:h2.poll-title (:title poll)]
+             (for [vote-opt-pair (map #(vector %1 %2)
+                                       (:options poll)
+                                       (partition-by :id (:votes poll)))]
+               [:div.option
+                [:span.option-text (:text (first vote-opt-pair))]
+                [:span.option-vote-count (count (second vote-opt-pair))]])])
+
+(defpartial poll-create-form []
+            (label "title" "Title: ")
+            (text-field "title")
+            (label "option" "Options (separate with commas): ")
+            (text-field "options"))
+
+(defpage "/polls/list/" []
+         (layout
+           (polls-list @poll/polls)))
+(defpage "/polls/list/:id" {:keys [id]}
+         (if id
+           (let [matches (filter #(= (:id %) (Integer/parseInt id)) @poll/polls )]
+             (if (empty? matches)
+               [:span.error "Poll with that ID does not exist"]
+               (poll-list @(first matches))))))
+
+(defpage [:get "/polls/create/"] []
+         (layout
+           (form-to [:post "/polls/create/"]
+                    (poll-create-form)
+                    (submit-button "Create"))))
+
+(defpage [:post "/polls/create/"] {:keys [title options]}
+         (let [options (clojure.string/split options #",*\s*,*")]
+           (if (poll/poll-exists? title)
+             (layout
+               [:span.error "Poll with that title already exists!"])
+             (do
+               (poll/add-poll! title options)
+               (redirect "/polls/list/")))))
+
